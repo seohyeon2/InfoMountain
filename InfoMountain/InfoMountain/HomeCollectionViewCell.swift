d676747f6737c3ba4589f916fb0e61a13ab81267
HyosungP <hyosing92@gmail.com>
2022-09-14 14:55:07 +0900

    feat: Cell 클릭 링크 추가
    - 링크추가
    - cell main label 추가

--- InfoMountain/InfoMountain/HomeCollectionViewCell.swift
+++ InfoMountain/InfoMountain/HomeCollectionViewCell.swift
@@ -10,4 +10,6 @@ import UIKit
 class HomeCollectionViewCell: UICollectionViewCell {
     
     @IBOutlet weak var titleLabel: UILabel!
+    
+    
 }
--- InfoMountain/InfoMountain/Links.swift
+++ InfoMountain/InfoMountain/Links.swift
@@ -9,7 +9,17 @@ import Foundation
 import UIKit
 
 struct Links {
+    var lables = ["주의사항", "준비물", "장비설명", "초급", "중급", "고급", "언택트 등산1", "언택트 등산2", "언택트 등산3"]
+    
     var linkDatas = [
         "https://user-images.githubusercontent.com/50102522/190065003-f2121b67-00f8-4025-b806-04531fd951b1.jpg",
+        "",
+        "",
+        "https://user-images.githubusercontent.com/50102522/190067856-d4afc6dc-9ab3-4084-afc1-319582537788.jpg",
+        "https://user-images.githubusercontent.com/50102522/190068779-9c09fa30-e033-4fe1-a857-4e306d74d854.jpg",
+        "https://user-images.githubusercontent.com/50102522/190070119-a37646fa-5331-4ef3-a8d4-1d6b54cb3e39.jpg",
+        "https://www.youtube.com/watch?v=LMsUjtbro9s",
+        "https://www.youtube.com/watch?v=MUVpFPRImgA",
+        "https://www.youtube.com/watch?v=qtYmGL8ePN0"
     ]
 }
